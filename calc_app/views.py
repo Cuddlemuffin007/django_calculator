06055ee9807dbaf1fec3c426ce198d156ee9384f
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-02-29 18:17:39 -0500

    cleaned up UI

--- calc_app/views.py
+++ calc_app/views.py
@@ -2,34 +2,33 @@ from django.shortcuts import render
 
 
 def calc_view(request):
-    try:
-        var1 = 0 if not request.POST.get('var1') else float(request.POST.get('var1'))
-        var2 = 0 if not request.POST.get('var2') else float(request.POST.get('var2'))
-    except(ValueError, TypeError):
-        return render(request, 'base.html', {
-            'result': 'Invalid input. Please enter only digits 0 - 9'
-        })
+    if request.method == 'POST':
+        try:
+            var1 = float(request.POST.get('var1'))
+            var2 = float(request.POST.get('var2'))
+        except(ValueError, TypeError):
+            return render(request, 'base.html', {
+                'result': 'Invalid input. Please enter only digits 0 - 9',
+            })
 
-    operator = request.POST.get('operators', None)
-    result = 0
-    operation = ''
+        operator = request.POST.get('operators', None)
 
-    if operator == 'add':
-        operation = '+'
-        result = var1 + var2
-    elif operator == 'sub':
-        operation = '-'
-        result = var1 - var2
-    elif operator == 'mul':
-        operation = '*'
-        result = var1 * var2
-    elif operator == 'div':
-        operation = '/'
-        result = var1 / var2
-    next_val = result
+        if operator == 'add':
+            operation = '+'
+            result = var1 + var2
+        elif operator == 'sub':
+            operation = '-'
+            result = var1 - var2
+        elif operator == 'mul':
+            operation = '*'
+            result = var1 * var2
+        elif operator == 'div':
+            operation = '/'
+            result = var1 / var2
 
-    return render(request, 'base.html', {'result': result,
-                                         'var1': var1,
-                                         'var2': var2,
-                                         'next_val': next_val,
-                                         'operation': operation})
+        return render(request, 'base.html', {'result': result,
+                                             'var1': var1,
+                                             'var2': var2,
+                                             'operation': operation})
+    else:
+        return render(request, 'base.html', {})

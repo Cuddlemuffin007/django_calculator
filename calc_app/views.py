9182d31bf994cacc89993d13658c61afd5698b18
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-02-29 16:31:56 -0500

    changed none comparison to if not statement

--- calc_app/views.py
+++ calc_app/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 def calc_view(request):
     try:
-        var1 = 0 if request.POST.get('var1') == None else float(request.POST.get('var1'))
-        var2 = 0 if request.POST.get('var2') == None else float(request.POST.get('var2'))
+        var1 = 0 if not request.POST.get('var1') else float(request.POST.get('var1'))
+        var2 = 0 if not request.POST.get('var2') else float(request.POST.get('var2'))
     except(ValueError, TypeError):
         return render(request, 'base.html', {
             'result': 'Invalid input. Please enter only digits 0 - 9'
